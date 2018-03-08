afba3ed74e7b6e8573fa16fc8e2d40adaac6ac92
jacknieland <jacknieland@gmail.com>
2018-03-07 18:39:49 -0800

    Update CheckPrime.py

--- CheckPrime.py
+++ CheckPrime.py
@@ -1,23 +1,36 @@
+
+# Gets integet from user
+
 def get_integer():
-	return int(raw_input("Please type a number and press ENTER  "))
+	return int(input("Please type a number and press ENTER  "))
 	
+# Checks if number is prime, and prints the factors of non-prime inputs
+
 def check_prime(x):
+	factor_list = []
 	prime = 0
-	for element in range(2,(x)):
-		if x % element == 0:
-			prime = 1
-		elif prime == 0:
-			continue
-	if prime == 0:
-		print("Your number is prime.")
+	if x > 15000000:
+		print("Your number is too large for your hardware to factor")
 	else:
-		print("Your number is non-prime.")
+		for element in range(2,(x)):
+			if x % element == 0:
+				factor_list.append(element)
+				prime = 1
+			elif prime == 0:
+				continue
+		if prime == 0:
+			print("Your number is prime.")
+		else:
+			print("Your number is non-prime.")
+			print("The factors of your number are " + str(factor_list))
+
+# Nests check_prime function into a loop that prompts user to restart
 
 def start_loop():	
 	while True:
 		check_prime(get_integer())
 		while True:
-			usr_inpt = raw_input("Would you like to try another number? (y/n)  ")
+			usr_inpt = input("Would you like to try another number? (y/n)  ")
 			if usr_inpt in ("y","n"):
 				break
 			print("Please try again")
@@ -27,9 +40,7 @@ def start_loop():
 			print("Thanks for using Jack's PrimeCheck!")
 			break
 		
-start_loop()
-
-				
+start_loop()	
 
 
 
